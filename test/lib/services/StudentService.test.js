864d58d64680dd53a880dc2b8a25741d11d109eb
danmtnezcas <danmtnezcas@outlook.com>
2022-05-18 15:05:15 -0500

    Modificando archivo StudentService.test.js, corrigiendo sintaxis

--- test/lib/services/StudentService.test.js
+++ test/lib/services/StudentService.test.js
@@ -2,7 +2,7 @@ const Reader = require("../../../lib/utils/Reader");
 const StudentService = require("../../../lib/services/StudentService");
 
 describe("Unit Tests for StudentService class", () => {
-    test("Get the explorers list, we should have the students from student.json", () => {
+    test("Get the students list, we should have the students from student.json", () => {
         const studentsList = Reader.readJsonFile("students.json");
         const students = StudentService.getStudents(studentsList);
         expect(students).toEqual(
