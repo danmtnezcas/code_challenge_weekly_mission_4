97317e83835f9d58c94b985d304cf87fa8f44958
danmtnezcas <danmtnezcas@outlook.com>
2022-05-18 14:31:53 -0500

    Agregando archivo StudentService.test.js con prueba para el método getStudents

---
+++ test/lib/services/StudentService.test.js
@@ -0,0 +1,17 @@
+const Reader = require("../../../lib/utils/Reader");
+const StudentService = require("../../../lib/services/StudentService");
+
+describe("Unit Tests for StudentService class", () => {
+    test("Get the explorers list, we should have the students from student.json", () => {
+        const studentsList = Reader.readJsonFile("students.json");
+        const students = StudentService.getStudents(studentsList);
+        expect(students).toEqual(
+            expect.arrayContaining([
+                expect.objectContaining({"name": "Warren", "credits": 508}), // This student is in students.json
+                expect.objectContaining({"name": "Lucinda", "credits": 677}), // This student is in students.json
+                expect.objectContaining({"name": "Fuentes", "credits": 210}), // This student is in students.json
+                //expect.objectContaining({"name": "asdfj"}) // This student isn't in students.json
+            ])
+        );
+    });
+});
