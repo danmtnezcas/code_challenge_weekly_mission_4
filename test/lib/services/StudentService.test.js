656cb265800e179944452007605893af5e054e69
danmtnezcas <danmtnezcas@outlook.com>
2022-05-19 01:26:32 -0500

    Modificando archivo StudentService.test.js, agregando prueba para el método filterByCredits

--- test/lib/services/StudentService.test.js
+++ test/lib/services/StudentService.test.js
@@ -23,4 +23,16 @@ describe("Unit Tests for StudentService class", () => {
         expect(emailsWithCertification).toContain("Sharlene@visualpartnership.xyz"); // This email has certification
         //expect(emailsWithCertification).toContain("Camacho@visualpartnership.xyz"); // This email hasn't certification
     });
+    test("Get the students list with credits greater than a condition, we should only have the students from student.json with credits greater than a condition", () => {
+        const studentsList = Reader.readJsonFile("students.json");
+        const credits = 500;
+        const studentsWithCreditsGreaterThan = StudentService.filterByCredits(studentsList, credits);
+        expect(studentsWithCreditsGreaterThan).toEqual(
+            expect.arrayContaining([
+                expect.objectContaining({"name": "Warren", "credits": 508}), // This student has enough credits
+                expect.objectContaining({"name": "Lucinda", "credits": 677}), // This student has enough creditsjson
+                //expect.objectContaining({"name": "Fuentes", "credits": 210}), // This student doesn't have enough credits
+            ])
+        );
+    });
 });
